30a7f7e70bed3da12b283c2d7ccfdf84903f3bcb
Mercsman <99054416+Mercsman@users.noreply.github.com>
2024-01-24 18:36:56 -0800

    Update main.js to resolve conflict

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
-import ProductListing from './ProductListing.mjs';
 import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
 
-const dataSource = new ProductData("tents")
-const productList = new ProductListing("tents", dataSource, ".product-list")
-productList.init()
+listing.init();
