b4ddb800e3b5f0d994c8382c8d12c22b6c32071a
Brother Nelson <nelsoned@byui.edu>
2024-01-25 18:39:22 -0700

    Various fixes

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -3,7 +3,7 @@
 import {getLocalStorage} from "./utils.mjs";
 
 function cartItemTemplate(item) {
-    return `<li class="cart-card divider">
+  return `<li class="cart-card divider">
         <a href="#" class="cart-card__image">
             <img src="${item.Image}" alt="${item.Name}"/>
         </a>
@@ -13,18 +13,17 @@ function cartItemTemplate(item) {
             <p class="cart-card__color">${item.Colors[0].ColorName}</p>
             <p class="cart-card__quantity">qty: 1</p>
             <p class="cart-card__price">$${item.FinalPrice}</p>
-    </li>`
-
+    </li>`;
 }
 
-export default class ShoppingCart{
-    constructor(key,parentSelector){
-        this.key = key;
-        this.parentSelector = parentSelector;
-    }
-    renderCartContents() {
-        const cartItems = getLocalStorage(this.key);
-        const htmlItems = cartItems.map((item)=> cartItemTemplate(item));
-        document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
-    }
+export default class ShoppingCart {
+  constructor(key, parentSelector) {
+    this.key = key;
+    this.parentSelector = parentSelector;
+  }
+  renderCartContents() {
+    const cartItems = getLocalStorage(this.key) || [];
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
+  }
 }
